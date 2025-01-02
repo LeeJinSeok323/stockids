8419da9479b1c91198bfd0b61af78a5595521b67
YJKBigBo <yjg0557@naver.com>
2025-01-02 16:44:25 +0900

    🔨feature : 크롤링한 뉴스 DB 저장

--- finalProject/src/main/java/finalProject/controller/CrawController.java
+++ finalProject/src/main/java/finalProject/controller/CrawController.java
@@ -1,6 +1,7 @@
 package finalProject.controller;
 
 import finalProject.domain.craw.CrawItem;
+import finalProject.service.craw.CrawDbSaveService;
 import finalProject.service.craw.CrawService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,13 @@ public class CrawController {
     @Autowired
     private CrawService crawService;
 
+    @Autowired
+    private CrawDbSaveService crawDbSaveService;
+
     @GetMapping("/craw/news")
     public List<CrawItem> getNews(@RequestParam String query) throws IOException {
         List<CrawItem> filteredItems = crawService.fetchNews(query);
+        crawDbSaveService.execute(filteredItems);
         return filteredItems;
     }
 }
