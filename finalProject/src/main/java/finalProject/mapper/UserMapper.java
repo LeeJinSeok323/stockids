16d37888abd082305d37495a55be16821af16a3a
JinseokLee <jinseoki10@gmail.com>
2025-01-03 16:53:36 +0900

    📝chore: user

--- finalProject/src/main/java/finalProject/mapper/UserMapper.java
+++ finalProject/src/main/java/finalProject/mapper/UserMapper.java
@@ -6,4 +6,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     public AuthInfoDTO loginSelect(String userId);
+    public String getUserNumById(String userId);
 }
