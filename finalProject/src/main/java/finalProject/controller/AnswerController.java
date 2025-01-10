2900a4b3ed8a40f3d334e3f6db19e4ed3396c7b4
khm263 <127173251+khm263@users.noreply.github.com>
2025-01-10 17:37:17 +0900

    🔨 feature : 답변 수정4

--- finalProject/src/main/java/finalProject/controller/AnswerController.java
+++ finalProject/src/main/java/finalProject/controller/AnswerController.java
@@ -4,10 +4,14 @@ import finalProject.command.AnswerCommand;
 import finalProject.domain.AuthInfoDTO;
 import finalProject.mapper.UserMapper;
 import finalProject.service.AutoNumService;
+import finalProject.service.inquire.AnswerDeleteService;
 import finalProject.service.inquire.AnswerWriteService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +25,8 @@ public class AnswerController {
 UserMapper userMapper;
 @Autowired
 AnswerWriteService answerWriteService;
+@Autowired
+AnswerDeleteService answerDeleteService;
 
     @PostMapping("answerWrite")
     public String answerWrite(HttpSession session, AnswerCommand answerCommand) {
@@ -29,5 +35,12 @@ AnswerWriteService answerWriteService;
         answerWriteService.execute(answerCommand);
         return "redirect:/inquire/inquireDetail/" + answerCommand.getInquireNum();
     }
+
+    @GetMapping("answerDelete/{inquireNum}")
+    public String answerDelete(@PathVariable("inquireNum") String inquireNum, Model model, HttpSession session){
+        answerDeleteService.execute(inquireNum);
+        return "redirect:/inquire/inquireDetial/" + inquireNum;
+    }
+
 
 }
